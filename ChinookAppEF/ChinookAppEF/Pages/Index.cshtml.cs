9bdc0f32f4e2071dd0e0f196eaf27d179c1f9d91
Don Wibier <don@anykey.nl>
2020-09-23 18:12:16 +0200

    NavigationAttribute implemented

--- ChinookAppEF/ChinookAppEF/Pages/Index.cshtml.cs
+++ ChinookAppEF/ChinookAppEF/Pages/Index.cshtml.cs
@@ -1,14 +1,20 @@
+using Library;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace ChinookAppEF.Pages {
-    public class IndexModel : PageModel {
-        public void OnGet() {
+namespace ChinookAppEF.Pages
+{
 
-        }
-    }
+	public class IndexModel : PageModel
+	{
+		public void OnGet()
+		{
+
+		}
+	}
 }
